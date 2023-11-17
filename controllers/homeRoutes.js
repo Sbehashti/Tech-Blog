6edce0f535e9061744af4e3525a934a611a91f81
Sier Behashti <sbehashti@gmail.com>
2023-11-16 22:13:41 -0800

    fixing errors part 2

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,97 +1,99 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { User, Blog, Comment } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { User, Blog, Comment } = require("../models");
 
-router.get('/', (req, res) => {
-    console.log(req.session);
+router.get("/", (req, res) => {
+  console.log(req.session);
 
-    Post.findAll({
+  Blog.findAll({
+    attributes: ["id", "title", "comment_content", "created_at"],
+    include: [
+      {
+        model: Comment,
         attributes: [
-            'id',
-            'title',
-            'comment_content',
-            'created_at'
-            ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_content', 'blog_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })  
-    .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+          "id",
+          "comment_content",
+          "blog_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("home", { posts, loggedIn: req.session.loggedIn });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('signup');
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
 });
 
-router.get('/blog/:id', (req, res) => {
-    Post.findOne({
-        where: {
-            id: req.params.id
-        },
+router.get("/blog/:id", (req, res) => {
+  Blog.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "comment_content", "created_at"],
+    include: [
+      {
+        model: Comment,
         attributes: [
-            'id',
-            'title',
-            'comment_content',
-            'created_at'
+          "id",
+          "comment_content",
+          "blog_id",
+          "user_id",
+          "created_at",
         ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_content', 'blog_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
 
-.then(dbPostData => {
-    if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
         return;
-    }
+      }
 
-    const post = dbPostData.get({ plain: true });
-    res.render('blog', { post, loggedIn: req.session.loggedIn });
+      const post = dbPostData.get({ plain: true });
+      res.render("blog", { post, loggedIn: req.session.loggedIn });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,107 +1,107 @@
-const { Comment } = require('../models');
+const { Comment } = require("../models");
 const commentData = [
-    {
-        comment_content: "This is a comment",
-        user_id: 1,
-        blog_id: 1
-    },
-    {
-        comment_content: "This is another comment",
-        user_id: 2,
-        blog_id: 2
-    },
-    {
-        comment_content: "This is a third comment",
-        user_id: 3,
-        blog_id: 3
-    },
-    {
-        comment_content: "This is a fourth comment",
-        user_id: 4,
-        blog_id: 4
-    },
-    {
-        comment_content: "This is a fifth comment",
-        user_id: 5,
-        blog_id: 5
-    }
-    {
-        comment_content: "This is a sixth comment",
-        user_id: 6,
-        blog_id: 6
-    }
+  {
+    comment_content: "This is a comment",
+    user_id: 1,
+    blog_id: 1,
+  },
+  {
+    comment_content: "This is another comment",
+    user_id: 2,
+    blog_id: 2,
+  },
+  {
+    comment_content: "This is a third comment",
+    user_id: 3,
+    blog_id: 3,
+  },
+  {
+    comment_content: "This is a fourth comment",
+    user_id: 4,
+    blog_id: 4,
+  },
+  {
+    comment_content: "This is a fifth comment",
+    user_id: 5,
+    blog_id: 5,
+  },
+  {
+    comment_content: "This is a sixth comment",
+    user_id: 6,
+    blog_id: 6,
+  },
 ];
 const seedComments = () => Comment.bulkCreate(commentData);
 module.exports = seedComments;
 
-const { Blog } = require('../models');
+const { Blog } = require("../models");
 const blogData = [
   {
     title: "First Blog",
     content: "This is the content of the first blog",
-    user_id: 1
+    user_id: 1,
   },
   {
     title: "Second Blog",
     content: "This is the content of the second blog",
-    user_id: 2
-  }
+    user_id: 2,
+  },
   {
     title: "Third Blog",
     content: "This is the content of the third blog",
-    user_id: 3
+    user_id: 3,
   },
   {
     title: "Fourth Blog",
     content: "This is the content of the fourth blog",
-    user_id: 4
+    user_id: 4,
   },
   {
     title: "Fifth Blog",
     content: "This is the content of the fifth blog",
-    user_id: 5
+    user_id: 5,
   },
   {
     title: "Sixth Blog",
     content: "This is the content of the sixth blog",
-    user_id: 6
-  }
+    user_id: 6,
+  },
 ];
 const seedBlogs = () => Blog.bulkCreate(blogData);
 module.exports = seedBlogs;
 
-const { User } = require('../models');
-    const userData = [
-      {
-        username: "user1",
-        email: "user1@email.com",
-        password: "password1"
-      },
-      {
-        username: "user2",
-        email: "user2@email.com",
-        password: "password2"
-      },
-      {
-        username: "user3",
-        email: "user3@email.com",
-        password: "password3"
-      },
-      {
-        username: "user4",
-        email: "user4@email.com",
-        password: "password4"
-      },
-      {
-        username: "user5",
-        email: "user5@email.com",
-        password: "password5"
-      },
-      {
-        username: "user6",
-        email: "user6@email.com",
-        password: "password6"
-      }
-    ];
-    const seedUsers = () => User.bulkCreate(userData);
-    module.exports = seedUsers;
+const { User } = require("../models");
+const userData = [
+  {
+    username: "user1",
+    email: "user1@email.com",
+    password: "password1",
+  },
+  {
+    username: "user2",
+    email: "user2@email.com",
+    password: "password2",
+  },
+  {
+    username: "user3",
+    email: "user3@email.com",
+    password: "password3",
+  },
+  {
+    username: "user4",
+    email: "user4@email.com",
+    password: "password4",
+  },
+  {
+    username: "user5",
+    email: "user5@email.com",
+    password: "password5",
+  },
+  {
+    username: "user6",
+    email: "user6@email.com",
+    password: "password6",
+  },
+];
+const seedUsers = () => User.bulkCreate(userData);
+module.exports = seedUsers;

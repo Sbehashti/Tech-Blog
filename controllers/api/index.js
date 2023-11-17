34157958e2c0acb36f1fc5e84f056ee9825bda79
Sier Behashti <sbehashti@gmail.com>
2023-11-16 21:46:23 -0800

    fine-tuning

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes.js');
-const dashboardRoutes = require('./dashboardRoutes.js');
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userRoutes');
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/blog', blogRoutes);
+router.use('/comment', commentRoutes);
+router.use('/user', userRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,4 @@
-const withAuth = require(req, res, next) => {
+const withAuth = (req, res, next) => {
   if (!req.session.logged_in) {
     res.redirect('/login');
   } else {
